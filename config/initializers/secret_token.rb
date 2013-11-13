f2c5a88efb08b28a8dfed88b0c779df267874633
Bob Rudin <bobrudin@yahoo.com>
2013-11-12 22:49:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstBobApp::Application.config.secret_key_base = '485863659f09ba7dada2f864deb3272bfd36cb694fa548fb64c9917097bed2426c874065fad245e14e265bc7f94f17d631783bdbe0d1aefebd4ff9d73e89a747'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstBobApp::Application.config.session_store :cookie_store, key: '_first_bob_app_session'
